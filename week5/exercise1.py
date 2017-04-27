d70741e10d6a363467588e4b661ae4378beeb74b
Ethan <ethan-gonzaga@hotmail.com>
2017-04-27 21:41:24 +1000

    fix docstrings some things not working

--- week5/exercise1.py
+++ week5/exercise1.py
@@ -32,6 +32,7 @@ import math
 # return a lit of countdown messages, much like in the bad function above.
 # It should say something different in the last message.
 def countdown(message, start, stop, completion_message):
+    """Countdown to start."""
     for number in range(9, 0, -1):
         print(message + str(number))
     print(completion_message)
@@ -47,22 +48,25 @@ def countdown(message, start, stop, completion_message):
 # The stub functions are made for you, and each one is tested, so this should
 # hand hold quite nicely.
 def calculate_hypotenuse(base, height):
-
+    """Calculate hypotenuse."""
     hypotenuse = math.sqrt(base**2 + height**2)
     return hypotenuse
 
 
 def calculate_area(base, height):
+    """Calcuate area."""
     area = (0.5 * int(base) * int(height))
     return area
 
 
 def calculate_perimeter(base, height):
+    """Calculate perimeter."""
     perimeter = (base + height + calculate_hypotenuse)
     return perimeter
 
 
 def calculate_aspect(base, height):
+    """Calculate aspect."""
     if height > base:
         return "tall"
     elif height == base:
@@ -74,6 +78,7 @@ def calculate_aspect(base, height):
 # Make sure you reuse the functions you've already got
 # Don't reinvent the wheel
 def get_triangle_facts(base, height, units="mm"):
+    """Get triangle facts."""
     return {"area": calculate_area,
             "perimeter": calculate_perimeter,
             "height": height,
@@ -100,6 +105,7 @@ def get_triangle_facts(base, height, units="mm"):
 # but with the values and shape that relate to the specific
 # triangle we care about.
 def tell_me_about_this_right_triangle(facts_dictionary):
+    """Ask right triangle."""
     tall = """
             {height}
             |
@@ -147,6 +153,7 @@ def triangle_master(base,
                     height,
                     return_diagram=False,
                     return_dictionary=False):
+    """Triangle master."""
     dictionary = get_triangle_facts(base, height)
     diagram = tell_me_about_this_right_triangle(dictionary)
     if return_diagram and return_dictionary:
@@ -160,12 +167,14 @@ def triangle_master(base,
 
 
 def wordy_pyramid():
+    """Wordy pyramid."""
     pyramid_list = list_of_words_with_lengths(range(3, 21, 2))
     pyramid_list2 = list_of_words_with_lengths(range(20, 3, -2))
     return (pyramid_list + pyramid_list2)
 
 
 def get_a_word_of_length_n(length):
+    """Word length."""
     baseURL = "http://www.setgetgo.com/randomword/get.php?len="
     if length >= 3 and isinstance(length, int):
         try:
@@ -176,7 +185,7 @@ def get_a_word_of_length_n(length):
 
 
 def list_of_words_with_lengths(list_of_lengths):
-
+    """Word length."""
     wordList = []
     for length in list_of_lengths:
         word = get_a_word_of_length_n(length)
